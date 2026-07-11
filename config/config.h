1e8c2e3aa154a5f80a4c616844b4ab7ebcd029cb
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-07-11 12:16:26 -0700

    feat(config): Set up SD card SPI pin configuration in config.h

--- config/config.h
+++ config/config.h
@@ -79,4 +79,10 @@ extern int getCurrentThemeFlavor();
 //   Lower = snappier transitions. LVGL default is 300 ms.
 #define DISPLAY_SWIPE_ANIM_MS 150
 
+// SD Card SPI Settings
+#define SD_CS_PIN 5
+#define SD_SCK_PIN 18
+#define SD_MISO_PIN 19
+#define SD_MOSI_PIN 23
+
 #endif // CONFIG_H
