9521b4e9a383a7393ab89319688f60c66d0d3311
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-07-13 20:22:35 -0700

    feat(button): implement ButtonManager logic

--- src/button_manager.cpp
+++ src/button_manager.cpp
@@ -8,9 +8,44 @@ ButtonManager::ButtonManager(uint8_t pin, uint32_t debounceDelayMs, uint32_t lon
 
 void ButtonManager::begin() {
     pinMode(_pin, INPUT);
+    // Initialize state
+    uint8_t initVal = digitalRead(_pin);
+    _lastPinState = initVal;
+    _stableState = initVal;
 }
 
 ButtonAction ButtonManager::update(unsigned long currentMillis) {
-    // Skeleton implementation
-    return ButtonAction::NONE;
+    uint8_t reading = digitalRead(_pin);
+    ButtonAction action = ButtonAction::NONE;
+
+    if (reading != _lastPinState) {
+        _lastTransitionTime = currentMillis;
+        _lastPinState = reading;
+    }
+
+    if ((currentMillis - _lastTransitionTime) >= _debounceDelay) {
+        if (reading != _stableState) {
+            _stableState = reading;
+            if (_stableState == LOW) {
+                _pressStartTime = _lastTransitionTime;
+                _longPressTriggered = false;
+            } else { // HIGH (released)
+                if (!_longPressTriggered) {
+                    if (_pressStartTime > 0 && (currentMillis - _pressStartTime) < _longPressDelay) {
+                        action = ButtonAction::SINGLE_PRESS;
+                    }
+                }
+                _pressStartTime = 0;
+            }
+        }
+    }
+
+    if (_stableState == LOW && !_longPressTriggered) {
+        if ((currentMillis - _pressStartTime) >= _longPressDelay) {
+            _longPressTriggered = true;
+            action = ButtonAction::LONG_PRESS;
+        }
+    }
+
+    return action;
 }
