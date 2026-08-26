97468c9d0f7b88c298d049806a303b131d2da1ec
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-08-26 12:28:32 -0700

    chore: update environments in workflows and env example, clean up test files

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-    int point_count = 24;
-    int major_cnt = 7;
-    for(int i=0; i<major_cnt; i++) {
-        int val = (i * (point_count - 1)) / (major_cnt - 1);
-        std::cout << "Tick " << i << " value: " << val << " (val%4=" << val%4 << ")\n";
-    }
-}
